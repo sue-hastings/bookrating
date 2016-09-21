37693d56f469fe58219940bc0c1ebd80db5e9d28
Susan <susan.adelokiki@andela.co>
2016-09-21 14:32:37 +0100

    User authentication

--- config/policies.js
+++ config/policies.js
@@ -19,33 +19,12 @@
 
 module.exports.policies = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions (`true` allows public     *
-  * access)                                                                  *
-  *                                                                          *
-  ***************************************************************************/
+  '*': ['isAuthorized'], // Everything resctricted here
+  'UsersController': {
+    'create': true // We dont need authorization here, allowing public access
+  },
 
-  // '*': true,
-
-  /***************************************************************************
-  *                                                                          *
-  * Here's an example of mapping some policies to run before a controller    *
-  * and its actions                                                          *
-  *                                                                          *
-  ***************************************************************************/
-	// RabbitController: {
-
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		// '*': false,
-
-		// For the action `nurture`, apply the 'isRabbitMother' policy
-		// (this overrides `false` above)
-		// nurture	: 'isRabbitMother',
-
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		// feed : ['isNiceToAnimals', 'hasRabbitFood']
-	// }
+  'AuthController': {
+    '*': true // We dont need authorization here, allowing public access
+  }
 };
