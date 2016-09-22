a26a5c0e14924278a27a49efef27d796441b76ef
Susan <susan.adelokiki@andela.co>
2016-09-22 02:23:24 +0100

    Modify UserController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,15 +1,19 @@
-var Users = '../models/User.js'
+var jwtToken = require('../services/jwToken.js');
+
 module.exports = {
     signup: function(req, res) {
         if (req.body.password !== req.body.confirmPassword) {
+           console.log(req.body.password, req.body.confirmPassword);
             return res.json(401, { err: 'Password does not match' })
         }
-        Users.create(req.body).exec(function(err, user) {
+        User.create({username: req.body.username, firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, password: req.body.password}).exec(function(err, user) {
+          console.log(err, 'err');
             if (err) {
-                return res.json(err.status, { err: err });
+                res.json(err.status, { err: err });
+                return;
             }
             if (user) {
-                res.json(200, { user: user, token: jwtToken.issue({ id: user.id }) });
+                res.json(200, { user: user, token: jwtToken.issue({ id: user.id })});
             }
         });
     }
