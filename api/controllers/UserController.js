5454efa794b36c4f3f6f8dd7ceec9d42e87705cc
Susan <susan.adelokiki@andela.co>
2016-09-22 20:18:51 +0100

    Modify UserController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
 var jwtToken = require('../services/jwToken.js');
 
 module.exports = {
     signup: function(req, res) {
         if (req.body.password !== req.body.confirmPassword) {
-           console.log(req.body.password, req.body.confirmPassword);
-            return res.json(401, { err: 'Password does not match' })
-=======
-module.exports = {
-    signup: function(req, res) {
-        if (req.body.password !== req.body.confirmPassword) {
             return res.json(401, { err: 'Password does not match' });
->>>>>>> b68442e92ac6612904ce943d10313519a4926027
         }
-        User.create({username: req.body.username, firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, password: req.body.password}).exec(function(err, user) {
-          console.log(err, 'err');
+        User.create({ username: req.body.username, firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, password: req.body.password }).exec(function(err, user) {
+            console.log(err, 'err');
             if (err) {
                 res.json(err.status, { err: err });
                 return;
             }
             if (user) {
-                res.json(200, { user: user, token: jwtToken.issue({ id: user.id })});
+                res.json(200, { user: user, token: jwtToken.issue({ id: user.id }) });
             }
         });
     }
