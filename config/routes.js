648da752b13bccb178f9e211d02a1323654cdcc0
Susan <susan.adelokiki@andela.co>
2016-09-27 15:01:48 +0100

    Modify files

--- config/routes.js
+++ config/routes.js
@@ -33,7 +33,7 @@ module.exports.routes = {
      ***************************************************************************/
 
     '/': {
-        view: 'homepage'
+        view: 'index'
     },
     'GET /books': {
         controller: 'BookController',
@@ -59,7 +59,7 @@ module.exports.routes = {
         controller: 'UserController',
         action: 'signup'
     },
-      'POST /users/login': {
+    'POST /users/login': {
         controller: 'AuthController',
         action: 'login'
     },
@@ -72,8 +72,8 @@ module.exports.routes = {
         action: 'rate'
     },
     'POST /reset': {
-      controller: 'BookController',
-      action: 'reset'
+        controller: 'BookController',
+        action: 'reset'
     }
 
 
@@ -87,25 +87,25 @@ module.exports.routes = {
      *                                                                          *
      ***************************************************************************/
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+     * etc. depending on your default view engine) your home page.              *
+     *                                                                          *
+     * (Alternatively, remove this and add an `index.html` file in your         *
+     * `assets` directory)                                                      *
+     *                                                                          *
+     ***************************************************************************/
 
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Custom routes here...                                                    *
+     *                                                                          *
+     * If a request to a URL doesn't match any of the custom routes above, it   *
+     * is matched against Sails route blueprints. See `config/blueprints.js`    *
+     * for configuration options and examples.                                  *
+     *                                                                          *
+     ***************************************************************************/
 
 };
